55d89fc46710b2c38198707f3e7e76823a5ddd3f
MyLight <leonx300@mail.ru>
2025-05-22 11:34:49 +1000

    2025-05-22_11-35

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,36 +15,29 @@
 // }
 
 
-//2025-05-20 ClassWork
-// Task 9. Task 12
+//2025-05-22 ClassWork
 
+// Arrays. Task 1 (maxElement)
+// let Arr = [3, 1, 102, 50, 35, 8, 7, 12, 65, 5, 10];
 
+// console.warn(Arr);
 
+// let maxElem = Arr[0];
 
-
-
-
-
-
-// let num = prompt("Number");
-
-// let counter = 1;
-
-// for (let i = 2; i <= num; i++) {
-//     counter /= i;
+// for (let i = 1; i < Arr.length; i++) {
+//     if (Arr[i] > maxElem) {
+//         maxElem = Arr[i];
+//     } 
 // }
 
-// console.log(counter);
-
-
-
+// console.warn(maxElem);
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
 // Arrays. Task 9
 // let arrA = [2, 7, 1, 8, 0, 11, 1, 7, 11, 7];
 // let arrB = [11, 7, 1];
-
 
 // console.log(arrA);
 // console.log(arrB);
@@ -69,22 +62,6 @@
 // Arr.reverse();
 // console.warn(Arr);
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-// Arrays. Task 10 (without special methods of the Array object)
-let Arr = [3, 1, 102, 50, 35, 8, 7, 12, 65, 5, 10];
-
-console.warn(Arr);
-
-let maxElem = Arr[0];
-
-for (let i = 1; i < Arr.length; i++) {
-    //if (Arr[i] == Arr[Arr.length - 1]) break;
-    if (Arr[i] < maxElem) {
-        maxElem = Arr[i];
-    } 
-}
-
-console.warn(maxElem);
 
 
 
@@ -116,3 +93,112 @@ console.warn(maxElem);
 // console.log(arrA);
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+// Task. Concat
+// let arrA = [2, 7, 1, 8, 0, 11, 1, 7, 11, 7];
+// let result = [];
+
+// console.log(result.concat(arrA, [0, 0, 1, 1, 101]));
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+// Task. Homework 2025-05-20
+document.addEventListener('DOMContentLoaded', () => {
+
+    let num = Number(document.querySelector('.series-elements-number-input').value);
+
+    calculateSeriesSum = num => {
+
+        let currentElem = 1;
+        let result = 0;
+        let series = "1 - ";
+        
+        for (let i = 2; i <= num; i++) {
+            if (i % 2 == 0) {
+                if (i !== num) {
+                    series += `1 / ${i}  + `
+                } else series += `1 / ${i}`;
+        
+                result += currentElem / (i * -1);
+        
+            } else {  
+                if (i != num) {
+                    series += `1 / ${i} - `;
+                } else {
+                    series += `1 / ${i}`
+                };
+                
+                result += currentElem / i;     
+            }
+        }
+        
+        series += ` ≈ ${result.toFixed(4)}`;
+        return series;
+    };
+    
+    let mouseClickHandler = () => {
+        console.log(calculateSeriesSum(num))
+    };
+
+    document.querySelector('.series-elements-number-button').addEventListener('click', mouseClickHandler, false);
+    
+
+}, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.write("1 - ");
+
+//document.querySelector('.series').textContent = "1 - ";
+
+//for (let i = 2; i <= N; i++) {
+
+
+
+//     if (i % 2 == 0) {
+//         if (i !== N) {document.write("1 / ", i, " + ")} else document.write("1 / ", i);
+//         result += currentElem / (i * -1);  
+//     } else {
+        
+//         if (i != N) {
+//             document.write("1 / ", i, " - ")
+//         } else {document.write("1 / ", i)}
+        
+//         result += currentElem / i;     
+//     }
+// }
+
+
+
+
+//document.write(" ≈ ", result.toFixed(4));
+
+
+//document.querySelector('.series').textContent = ` ≈ ${result.toFixed(4)}`;
